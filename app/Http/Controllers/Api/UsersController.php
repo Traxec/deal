ca5833d21af588b1aa69bd89f75e85a175395650
cuidenghong <cui13203836651@gmail.com>
2017-08-25 15:50:01 +0800

    ceshi

--- app/Http/Controllers/Api/UsersController.php
+++ app/Http/Controllers/Api/UsersController.php
@@ -42,7 +42,6 @@ class UsersController extends Controller
     $data['status'] = 1;
 
     $user = User::create($data);
-    dd('1');
     $success['token'] =  $user->createToken('MyApp')->accessToken;
     $success['id'] =  $user->id;
     return response()->json(['success'=>$success], $this->successStatus);
